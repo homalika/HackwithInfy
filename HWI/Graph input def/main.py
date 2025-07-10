cd5470670edbb89156bf6c2c46477686179d05a2
Homalika Potnuru <130860491+homalika@users.noreply.github.com>
2025-07-10 23:15:41 +0530

    Delete HWI/Graph input def directory

--- HWI/Graph input def/main.py
+++
@@ -1,8 +0,0 @@
-n, m = map(int, input().split())  # Read number of provinces and roads
-graph = {i: [] for i in range(1, n+1)}  # Initialize adjacency list
-print(graph)
-for _ in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)  # Because roads are bidirectional
-print(graph)
